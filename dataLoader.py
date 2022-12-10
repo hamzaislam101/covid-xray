fc6eb62d79a9ef79dc855e86ec69c353a328129a
Hamza Islam <hamzaislam101@gmail.com>
2022-12-10 17:35:24 +0300

    updated data loader, trained the model, got 86.61

--- dataLoader.py
+++ dataLoader.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
 import torch
-from torchvision.io import read_image
+from PIL import Image
 
 class CustomImageDataset(torch.utils.data.Dataset):
     def __init__(self, img_dir, transform=None, target_transform=None):
@@ -14,7 +14,11 @@ class CustomImageDataset(torch.utils.data.Dataset):
             path = os.path.join(img_dir, label)
             path = os.path.join(path, 'images')
             for img in os.listdir(path):
-                self.img_labels = self.img_labels.append({'image': os.path.join(label+"/images", img), 'label': label}, ignore_index=True)
+                if self.img_labels.empty:
+                    self.img_labels = pd.DataFrame([[os.path.join(label+"/images", img), label]],columns=['image', 'label'])
+                else:
+                    imageLabel = pd.DataFrame([[os.path.join(label+"/images", img), label]],columns=['image', 'label'])
+                    self.img_labels = pd.concat([self.img_labels,imageLabel], ignore_index=True)
 
         self.img_dir = img_dir
         self.transform = transform
@@ -22,14 +26,26 @@ class CustomImageDataset(torch.utils.data.Dataset):
 
     def __len__(self):
         return len(self.img_labels)
+    
+    def getImageLabelAsInteger(self, label):
+        if label == 'COVID':
+            return 0
+        elif label == 'Lung_Opacity':
+            return 1
+        elif label == 'Normal':
+            return 2
+        elif label == 'Viral Pneumonia':
+            return 3
+        else:
+            return -1
 
     def __getitem__(self, idx):
         img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
-        print(img_path)
-        image = read_image(img_path)
+        image = Image.open(img_path)
+        image = image.convert('RGB')
         label = self.img_labels.iloc[idx, 1]
         if self.transform:
             image = self.transform(image)
         if self.target_transform:
             label = self.target_transform(label)
-        return image, label
+        return image, self.getImageLabelAsInteger(label)
--- train.py
+++ train.py
@@ -1,61 +1,153 @@
-#import pytorch and other libraries
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-from torch.utils.data import DataLoader
-from torchvision import datasets, transforms, models
+from torchvision import datasets, transforms
+from torchvision import models
+from torch import nn
+from torch import optim
+from dataLoader import CustomImageDataset
 
-batch_size = 16
 
-#read the images from COVID-19 Radiography Dataset folder with images in the labeled folders
-train_dataset = datasets.ImageFolder(root="COVID-19_Radiography_Dataset", transform=transforms.ToTensor())
-train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
 
-#read the images from COVID-19 Radiography Dataset folder with images in the labeled folders
-test_dataset = datasets.ImageFolder(root="COVID-19_Radiography_Dataset", transform=transforms.ToTensor())
-test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=True)
-
-transforms_train = transforms.Compose([
-    transforms.Resize((224, 224)),   #must same as here
-    transforms.RandomResizedCrop(224),
-    transforms.RandomHorizontalFlip(), # data augmentation
-    transforms.ToTensor(),
-    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]) # normalization
-])
-transforms_test = transforms.Compose([
-    transforms.Resize((224, 224)),   #must same as here
-     transforms.CenterCrop((224, 224)),
+# Define the transforms for the images
+data_transforms = transforms.Compose([
+    transforms.Resize((224, 224)),
     transforms.ToTensor(),
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 ])
 
+data = CustomImageDataset("COVID-19_Radiography_Dataset",data_transforms)
 
-#check the number of classes
-classes = train_dataset.classes
-print(classes)
+# Split the dataset into train, train, and validation sets
+train_size = int(0.7 * len(data))
+val_size = int(0.15 * len(data))
+test_size = len(data) - train_size - val_size
 
-#check the number of classes
-classes = test_dataset.classes
-print(classes)
+train_data, val_data, test_data = torch.utils.data.random_split(
+    data,
+    [train_size, val_size, test_size]
+)
 
-#use the resnet18 model from pytorch models
-model = models.resnet18(pretrained=True)
 
-#add a new fully connected layer with 4 outputs
-model.fc = nn.Linear(512, 4)
+# Define the data loaders
+train_loader = torch.utils.data.DataLoader(
+    train_data,
+    batch_size=64,
+    shuffle=True
+)
+val_loader = torch.utils.data.DataLoader(
+    val_data,
+    batch_size=64,
+    shuffle=False
+)
+test_loader = torch.utils.data.DataLoader(
+    test_data,
+    batch_size=64,
+    shuffle=False
+)
 
-#provide input to the model
-x = torch.randn(16, 3, 224, 224)
-print(model(x).shape)
+# Define the classes
+classes = ['COVID', 'Lung_Opacity', 'Normal', 'Viral Pneumonia']
 
-#compile the model
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+# Load the pre-trained ResNet50 model
+model = models.resnet50(pretrained=True)
 
-#train the model
-model.fit(train_loader, epochs=10)
+# Freeze the weights of the pre-trained model
+for param in model.parameters():
+    param.requires_grad = False
 
-#evaluate the model
-loss_0, acc_0 = model.evaluate(test_loader)
+# Define the additional layers
+model.fc = nn.Sequential(
+    nn.Linear(in_features=2048, out_features=1024),
+    nn.ReLU(),
+    nn.Dropout(p=0.5),
+    nn.Linear(in_features=1024, out_features=512),
+    nn.ReLU(),
+    nn.Dropout(p=0.5),
+    nn.Linear(in_features=512, out_features=len(classes))
+)
 
-print("loss "+str(loss_0)+" acc "+str(acc_0))
+# Define the loss function and the optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters())
+
+#move the model to the GPU
+device = torch.device("mps")
+model.to(device)
+
+def train_model(model, criterion, optimizer, train_loader, val_loader, num_epochs=30):
+    for epoch in range(num_epochs):
+        # Training phase
+        model.train()
+        train_loss = 0.0
+        for inputs, labels in train_loader:
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+            # Zero the gradients
+            optimizer.zero_grad()
+            # Forward pass
+            outputs = model(inputs)
+            # Compute the loss
+            loss = criterion(outputs, labels)
+            # Backward pass
+            loss.backward()
+            # Update the weights
+            optimizer.step()
+            # Update the training loss
+            train_loss += loss.item() * inputs.size(0)
+            print(f'train_loss: {train_loss}')
+
+        # Validation phase
+        model.eval()
+        val_loss = 0.0
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            for inputs, labels in val_loader:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+                # Forward pass
+                outputs = model(inputs)
+                # Compute the loss
+                loss = criterion(outputs, labels)
+                # Update the validation loss
+                val_loss += loss.item() * inputs.size(0)
+                # Get the predicted labels
+                _, predicted = torch.max(outputs, 1)
+                # Update the total number of correct predictions
+                correct += (predicted == labels).sum().item()
+                # Update the total number of images
+                total += labels.size(0)
+
+        # Print the epoch statistics
+        print(f'Epoch: {epoch+1}/{num_epochs}')
+        print(f'Training Loss: {train_loss/len(train_loader.dataset):.4f}')
+        print(f'Validation Loss: {val_loss/len(val_loader.dataset):.4f}')
+        print(f'Validation Accuracy: {correct/total:.4f}')
+
+# Train the model
+train_model(model, criterion, optimizer, train_loader, val_loader)
+
+# Test the trained model
+model.eval()
+test_loss = 0.0
+correct = 0
+total = 0
+with torch.no_grad():
+    for inputs, labels in test_loader:
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+        # Forward pass
+        outputs = model(inputs)
+        # Compute the loss
+        loss = criterion(outputs, labels)
+        # Update the test loss
+        test_loss += loss.item() * inputs.size(0)
+        # Get the predicted labels
+        _, predicted = torch.max(outputs, 1)
+        # Update the total number of correct predictions
+        correct += (predicted == labels).sum().item()
+        # Update the total number of images
+        total += labels.size(0)
+
+# Print the test statistics
+print('Test Loss:', test_loss/len(test_loader.dataset))
+print('Test Accuracy:', correct/total)
