965d788f2c34a00260924cd4f765f403c1de2ecd
Hamza Islam <hamzaislam101@gmail.com>
2022-12-11 16:48:32 +0300

    generate shuffled pixel images

--- shufflePixels.py
+++ shufflePixels.py
@@ -1,33 +1,43 @@
 from PIL import Image
 import matplotlib.pyplot as plt
 import math
-
-im = Image.open("COVID-19_Radiography_Dataset/COVID/images/COVID-11.png", "r")
-arr = im.load() #pixel data stored in this 2D array
+import os
 
 def rot(A, n, x1, y1): #this is the function which rotates a given block
     temple = []
     for i in range(n):
         temple.append([])
         for j in range(n):
-            temple[i].append(arr[x1+i, y1+j])
+            temple[i].append(A[x1+i, y1+j])
     for i in range(n):
         for j in range(n):
-            arr[x1+i,y1+j] = temple[n-1-i][n-1-j]
+            A[x1+i,y1+j] = temple[n-1-i][n-1-j]
 
 
-xres = 299
-yres = 299
-BLKSZ = 12 #blocksize
-for i in range(2, BLKSZ+1):
-    for j in range(int(math.floor(float(xres)/float(i)))):
-        for k in range(int(math.floor(float(yres)/float(i)))):
-            rot(arr, i, j*i, k*i)
-for i in range(3, BLKSZ+1):
-    for j in range(int(math.floor(float(xres)/float(BLKSZ+2-i)))):
-        for k in range(int(math.floor(float(yres)/float(BLKSZ+2-i)))):
-            rot(arr, BLKSZ+2-i, j*(BLKSZ+2-i), k*(BLKSZ+2-i))
+def shuffleImage(path,number):
+    im = Image.open(path, "r")
+    arr = im.load() #pixel data stored in this 2D array
+    xres = 299
+    yres = 299
+    BLKSZ = 9 #blocksize
+    for i in range(2, BLKSZ+1):
+        for j in range(int(math.floor(float(xres)/float(i)))):
+            for k in range(int(math.floor(float(yres)/float(i)))):
+                rot(arr, i, j*i, k*i)
+    for i in range(3, BLKSZ+1):
+        for j in range(int(math.floor(float(xres)/float(BLKSZ+2-i)))):
+            for k in range(int(math.floor(float(yres)/float(BLKSZ+2-i)))):
+                rot(arr, BLKSZ+2-i, j*(BLKSZ+2-i), k*(BLKSZ+2-i))
 
-plt.figure(0,figsize = (6, 6))
-plt.imshow(im,cmap='gray')
-plt.show()
+    #plt.figure(0,figsize = (6, 6))
+    #plt.imshow(im,cmap='gray')
+    im.save("shuffledImages/shuffled-"+str(number)+".png")
+    #plt.show()
+
+os.mkdir("shuffledImages")
+x = 1
+for img in os.listdir("COVID-19_Radiography_Dataset/COVID/images"):
+    if x == 100:
+        break
+    shuffleImage("COVID-19_Radiography_Dataset/COVID/images/" + img,x)
+    x += 1
